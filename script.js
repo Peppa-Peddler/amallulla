98e69f45c65385965ffa1ed0cb810fda7cf4a645
peppa-peddler <peppasama@gmail.com>
2021-04-20 15:13:50 -0500

    addingmenu

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ $('.voption').click(function(){
     $(this).addClass("selected");
     $(this).parent().next().find("iframe").attr("src",$(this).data("src"));
 })
+
+$('.ventana').click(function(){
+    $('.ventana').removeClass("selected");
+    $(this).addClass("selected");
+    $('.recursos').hide();
+    $('.recursos-'+$(this).data("option")).show();
+})
